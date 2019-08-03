f2e08d27461c09063700d09a291c99315512be1b
jerryguo255 <jerryguo255@outlook.com>
2019-08-03 22:39:51 +1200

    add Order, Volunteer Controller

---
+++ bellyful_proj/Migrations/20190803095351_changeUserRole.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bellyful_proj.Migrations
+{
+    public partial class changeUserRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
